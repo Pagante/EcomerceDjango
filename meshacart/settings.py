d5fb45d37bcf3a829f28c78f233c29fa78d1d1ca
Pagante <55301195+Pagante@users.noreply.github.com>
2021-09-16 07:07:49 +0100

    adding .ebextension config file

--- meshacart/settings.py
+++ meshacart/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['meshakart-env.eba-cwppkwxx.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['MeshaPro.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
