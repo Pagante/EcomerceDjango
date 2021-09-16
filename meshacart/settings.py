28d325e0fc6da3e86854a64c1d9a3cfd84caf018
Pagante <55301195+Pagante@users.noreply.github.com>
2021-09-16 04:59:35 +0100

    Edit

--- meshacart/settings.py
+++ meshacart/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['meshakart-env.eba-cwppkwxx.us-west-2.elasticbeanstalk.com', '127.0.0.1']
+ALLOWED_HOSTS = ['meshakart-env.eba-cwppkwxx.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
